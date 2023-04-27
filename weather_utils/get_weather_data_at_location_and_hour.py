6df4034a1d1d8d2b4d25665a1c14a38d2476487d
Jian Gong <164676+GoJian@users.noreply.github.com>
2023-04-27 01:13:00 -0600

    working code for weather data API check in.

--- weather_utils/get_weather_data_at_location_and_hour.py
+++ weather_utils/get_weather_data_at_location_and_hour.py
@@ -17,9 +17,10 @@ def local_time_to_utc_str(dt_str, tz_str):
 
     Returns:
         str: A datetime string in UTC in the format 'YYYYMMDDHH'.
+        int: The time difference in hours UTC - local time.
     """
     # Parse the datetime string into a datetime object
-    dt_local = datetime.strptime(dt_str, '%Y-%m-%d %H:%M')
+    dt_local = datetime.strptime(dt_str, '%Y-%m-%dT%H:%M')
 
     # Create a timezone object from the given timezone string
     tz = pytz.timezone(tz_str)
@@ -27,13 +28,16 @@ def local_time_to_utc_str(dt_str, tz_str):
     # Localize the datetime object to the given timezone
     dt_tz = tz.localize(dt_local)
 
+    # Calculate the time difference from the current UTC time
+    time_diff_hrs = int( dt_tz.utcoffset().total_seconds() / 3600 )
+
     # Convert the datetime object to UTC
     dt_utc = dt_tz.astimezone(pytz.utc)
-
+    
     # Format the UTC datetime object as a string
     dt_utc_str = dt_utc.strftime("%Y%m%d%H")
 
-    return dt_utc_str
+    return dt_utc_str, time_diff_hrs
 
 # ==================================================================================
 
@@ -111,7 +115,7 @@ from datetime import datetime, timedelta
 from urllib.parse import urlencode
 import json
 
-def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_hour=None):
+def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_hour_str, timezone_diff_hrs, get_24hour_data=False):
     """
     Fetches upper air wind speed and direction data from the GFS Model
     for the specified latitude, longitude, altitude, and hour.
@@ -122,18 +126,18 @@ def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_h
         latitude (float): The latitude in decimal degrees format.
         longitude (float): The longitude in decimal degrees format.
         altitude (int): The altitude in meters.
-        query_hour (str): The forecast hour in YYYYMMDDHH format. Defaults to the current UTC time.
+        query_hour_str (str): The forecast hour in YYYYMMDDHH format. Defaults to the current UTC time.
 
     Returns:
         wind_speed (float): The wind speed in meters per second.
         wind_direction (float): The wind direction in degrees.
     """
     # Set the query hour to the current UTC hour if not specified
-    if query_hour is None:
+    if query_hour_str is None:
         query_hour_dt = datetime.utcnow().strftime('%Y%m%d%H')
     else:
         # Parse the forecast time string into a datetime object
-        query_hour_dt = datetime.strptime(query_hour, '%Y%m%d%H')
+        query_hour_dt = datetime.strptime(query_hour_str, '%Y%m%d%H')
 
         # Calculate the time difference from the current UTC time
         time_diff = query_hour_dt - datetime.utcnow()
@@ -151,27 +155,24 @@ def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_h
             query_hour = query_hour_dt.strftime('%Y%m%d%H')
             print('Warning: Forecast time limited to 16 days from the current UTC time.')
 
-    # Calculate the pressure at the specified altitude
-    pressure_hpa = pressure_at_altitude(altitude)/100
-    # Table from https://open-meteo.com/en/docs/gfs-api
-    lookup_pressure = np.array([1000,975,950,925,900,850,800,750,700,650,600,550,500,450,400,350,300,250,200,150,100,70,50,40,30,20,15,10])
-
-    # Find the nearest pressure level
-    nearest_idx = np.abs(lookup_pressure - pressure_hpa).argmin()
-    nearest_pressure = lookup_pressure[nearest_idx]
-
-    query_windspeed_air_string = f'windspeed_{nearest_pressure}hPa'
-    query_winddirection_air_string = f'winddirection_{nearest_pressure}hPa'
-
-    query_windspeed_ground_string = 'windspeed_10m'
+    # Surface level parameters
     query_temperature_ground_string = 'temperature_2m'
     query_relativehumidity_ground_string = 'relativehumidity_2m'
+    query_surface_pressure_string = 'surface_pressure'
+    query_cloudcover_string = 'cloudcover'
+    query_direct_radiation_string = 'direct_radiation'
+    quert_precipitation_string = 'precipitation'
+    query_windspeed_ground_string = 'windspeed_10m'
+    query_winddirection_ground_string = 'winddirection_10m'
 
     hourly_params = [query_temperature_ground_string, 
-                     query_relativehumidity_ground_string, 
+                     query_relativehumidity_ground_string,
+                     query_surface_pressure_string,
+                     query_cloudcover_string,
+                     query_direct_radiation_string,
+                     quert_precipitation_string,
                      query_windspeed_ground_string,
-                     query_windspeed_air_string,
-                     query_winddirection_air_string]
+                     query_winddirection_ground_string]
 
     # Define the API endpoint URL
     url_template_forecast = 'https://api.open-meteo.com/v1/gfs?{params}'
@@ -185,7 +186,7 @@ def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_h
         'start_date': query_hour_dt.strftime('%Y-%m-%d'),
         'end_date': query_hour_dt.strftime('%Y-%m-%d'),
         'hourly': hourly_params,
-        'current_weather': 'true'
+        'current_weather': 'false'
     }
 
     # Determine if the forecast time is in the future or past
@@ -195,16 +196,6 @@ def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_h
         # Switch to access historical data
         url_template_past = 'https://archive-api.open-meteo.com/v1/archive?{params}'
 
-        # Find the nearest altitude (only 100m and 10m are available from GFS)
-        lookup_past_altitude = np.array([100, 10])
-        nearest_idx = np.abs(lookup_past_altitude - altitude).argmin()
-        nearest_altitude = lookup_past_altitude[nearest_idx]
-
-        query_windspeed_string = f'windspeed_{nearest_altitude}m'
-        query_winddirection_string = f'winddirection_{nearest_altitude}m'
-
-        hourly_params = [query_temperature_ground_string, query_windspeed_string, query_winddirection_string]
-
         params_past = {
             'latitude': str(latitude),
             'longitude': str(longitude),
@@ -212,7 +203,7 @@ def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_h
             'start_date': query_hour_dt.strftime('%Y-%m-%d'),
             'end_date': query_hour_dt.strftime('%Y-%m-%d'),
             'hourly': hourly_params,
-            'current_weather': 'true'}
+            'current_weather': 'false'}
         
         url = url_template_past.format(params=urlencode(params_past, doseq=True))
 
@@ -225,6 +216,8 @@ def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_h
     # Debug check response
     #print(response.status_code)
     #print(json.dumps(json.loads(response.content), indent=2))
+
+    timedelta_hours = timedelta(hours = timezone_diff_hrs)
 
     if response.status_code == 200:
         # Extract the wind speed and direction data from the response JSON
@@ -242,32 +235,49 @@ def get_weather_data_at_location_and_hour(latitude, longitude, altitude, query_h
 
         temp = np.nan
         rel_humidity = np.nan
+        pressure = np.nan
+        cloud_cover = np.nan
+        direct_radiation = np.nan
+        precipitation = np.nan
         wind_speed_ground = np.nan
-        wind_speed_air = np.nan
-        wind_dir_air = np.nan
+        wind_dir_ground = np.nan
 
         if len(time_idx) > 0:
             # Extract the wind speed and direction data for the matching time
-            temp = data['hourly'][query_temperature_ground_string][time_idx[0]]
-
-            if isForecast:
-                rel_humidity = data['hourly'][query_relativehumidity_ground_string][time_idx[0]]
-                wind_speed_ground = data['hourly'][query_windspeed_ground_string][time_idx[0]]
-                wind_speed_air = data['hourly'][query_windspeed_air_string][time_idx[0]]
-                wind_dir_air = data['hourly'][query_winddirection_air_string][time_idx[0]]
+            if get_24hour_data:
+                time_utc = data['hourly']['time']
+                time_local = [(datetime.strptime(t, '%Y-%m-%dT%H:%M') + timedelta_hours).strftime('%Y-%m-%dT%H:%M') for t in time_utc]
+                temp = data['hourly'][query_temperature_ground_string]
+                rel_humidity = data['hourly'][query_relativehumidity_ground_string]
+                pressure = data['hourly'][query_surface_pressure_string]
+                cloud_cover = data['hourly'][query_cloudcover_string]
+                direct_radiation = data['hourly'][query_direct_radiation_string]
+                precipitation = data['hourly'][quert_precipitation_string]
+                wind_speed_ground = data['hourly'][query_windspeed_ground_string]
+                wind_dir_ground = data['hourly'][query_winddirection_ground_string]
             else:
-                wind_speed_air = data['hourly']['windspeed_100m'][time_idx[0]]
-                wind_dir_air = data['hourly']['winddirection_100m'][time_idx[0]]
+                time_utc = data['hourly']['time'][time_idx[0]]
+                time_local = (datetime.strptime(time_utc, '%Y-%m-%dT%H:%M') + timedelta_hours).strftime('%Y-%m-%dT%H:%M')
+                temp = data['hourly'][query_temperature_ground_string][time_idx[0]]
+                rel_humidity = data['hourly'][query_relativehumidity_ground_string][time_idx[0]]
+                pressure = data['hourly'][query_surface_pressure_string][time_idx[0]]
+                cloud_cover = data['hourly'][query_cloudcover_string][time_idx[0]]
+                direct_radiation = data['hourly'][query_direct_radiation_string][time_idx[0]]
+                precipitation = data['hourly'][quert_precipitation_string][time_idx[0]]
+                wind_speed_ground = data['hourly'][query_windspeed_ground_string][time_idx[0]]
+                wind_dir_ground = data['hourly'][query_winddirection_ground_string][time_idx[0]]
 
-        # Convert wind direction from 0 to 360 to -180 to 180
-        wind_dir_air = (wind_dir_air + 180) % 360 - 180
-
-        # Return the weather data in a dictionary
-        weather_dict = {'temp': temp, 
-                        'rel_humidity': rel_humidity, 
-                        'wind_speed_ground': wind_speed_ground, 
-                        'wind_speed_air': wind_speed_air, 
-                        'wind_dir_air': wind_dir_air}
+        weather_dict = {'forecast': isForecast,
+                        'time_UTC': time_utc,
+                        'time_local': time_local,
+                        'temp_C': temp, 
+                        'rel_humidity_%': rel_humidity,
+                        'surface_pressure_hPa': pressure,
+                        'cloud_cover_%': cloud_cover,
+                        'direct_radiation_W/m2': direct_radiation,
+                        'precipitation_mm': precipitation,
+                        'wind_speed_ground_Km/h': wind_speed_ground,
+                        'wind_dir_ground_deg': wind_dir_ground}
         
         return weather_dict
     else:
