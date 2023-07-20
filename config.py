b45d90a174198d48f639fb536cf959cb28e2f77a
Luke Macy <lukemacy1@gmail.com>
2023-07-20 01:00:50 -0600

    ingested hvaclab data, added add_feature util, small fixes

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
 import json, sys, warnings, os
 from modules.utils.get_file_names import get_file_names
 from modules.logging_methods.main import setup_logger
+from modules.utils.get_add_features import get_add_features
 
 def load_config(job_id):
   setup_logger(job_id or 0)
 
   path = '.'
-  data_path = f"{path}/clean_data_extended"
+  data_path = f"{path}/data"
   tmp_path = f"{path}/models/tmp"
-  file_list = get_file_names(data_path)
   results_header = ['model_type', 'bldgname', 'y_column', 'imputation_method', 'feature_method', 'n_feature', 'updated_n_feature', 'time_step', 'datelevel', 'rmse', 'mae', 'mape', 'model_file', 'model_data_path', 'building_file']
-  y_column = ['present_elec_kwh', 'present_htwt_mmbtu', 'present_wtr_usgal', 'present_chll_tonhr', 'present_co2_tons']
-  add_feature = ['temp_c', 'rel_humidity_%', 'surface_pressure_hpa', 'cloud_cover_%', 'direct_radiation_w/m2', 'precipitation_mm', 'wind_speed_ground_km/h', 'wind_dir_ground_deg']
+  y_column = ['total_ele (kw)']
+  exclude_column = ['']
+  exclude_file = [""]
+  file_path = f"{data_path}/data.csv"
+  file_list = get_file_names(data_path, exclude_file)
+  add_feature = get_add_features(file_path, y_column)
   header = ['ts'] + y_column + add_feature
   n_feature = list(range(1, len(add_feature)))  # start from 1 because sliding window technique will count as a feature
 
@@ -21,10 +25,10 @@ def load_config(job_id):
     "data_path": data_path,
     "tmp_path": tmp_path,
     "building_file": file_list,
-    "exclude_file": ["Summary_Report_Extended.csv"],
-    "exclude_column": ["present_co2_tons"],
+    "exclude_file": exclude_file,
+    "exclude_column": exclude_column,
     "update_add_feature": False,
-    "save_preprocessed_file": False,
+    "save_preprocessed_file": True,
     "save_model_file": False,
     "save_model_plot": False,
     "save_at_each_delta": True,
@@ -37,11 +41,7 @@ def load_config(job_id):
     "header": header,
     "results_header": results_header,
     "y_column_mapping": {
-      'present_elec_kwh': 'electricity',
-      'present_htwt_mmbtu': 'hot_water',
-      'present_wtr_usgal': 'water',
-      'present_chll_tonhr': 'chilled_water',
-      'present_co2_tons': 'co2_emissions'
+      'total_ele (kw)': 'electricity',
     },
     
     # preprocessing/training scope
@@ -49,10 +49,10 @@ def load_config(job_id):
     "imputation_method": ["linear_regression","linear_interpolation", "prophet", "lstm"],
     "feature_method": ["rfecv", "lassocv"],
     "datelevel": ["hour"],
-    "time_step": [24],            # window size of the sliding window technique and unit length of forecasts
-    "train_test_split": 0.7,
-    "train_ratio_threshold": 0.7, # minimum percent non-nans in training set
-    "test_ratio_threshold": 0.7,  # minimum percent non-nans in testing set
+    "time_step": [1],            # window size of the sliding window technique and unit length of forecasts
+    "train_test_split": 0.8,
+    "train_ratio_threshold": 0.8, # minimum percent non-nans in training set
+    "test_ratio_threshold": 0.8,  # minimum percent non-nans in testing set
         
     # hyperparameters
     "n_feature": n_feature,
