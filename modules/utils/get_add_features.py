c8c938413f7b93c9ff5822c355363f1f031290eb
Luke Macy <lukemacy1@gmail.com>
2023-07-26 08:33:13 -0600

    updated configs to exclude unused columns from training

--- modules/utils/get_add_features.py
+++ modules/utils/get_add_features.py
@@ -6,7 +6,7 @@ def get_add_features(file_path, exclude_column):
         header_row = next(reader)  # Read the first row (header)
 
         if exclude_column is not None and isinstance(exclude_column, list):
-            exclude_columns = exclude_column + ['ts', 'bldgname']
+            exclude_columns = exclude_column
             header_row = [column for column in header_row if column not in exclude_columns]
 
         return header_row
