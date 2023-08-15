8aa4c0644dbaf2f4f133297ed1bdae7ad02e0332
Luke Macy <lukemacy1@gmail.com>
2023-08-14 19:51:52 -0600

    updated surface pressure column, added logic to fix 7 hour timezone offset

--- flask_app/weather_utils/main.py
+++ flask_app/weather_utils/main.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 from .build_extended_datafile_main import *
 
 def build_extended_clean_data(config):
@@ -9,7 +9,10 @@ def build_extended_clean_data(config):
     clean_data_path = config['clean_data_path']
   
     for file_name in sorted(os.listdir(clean_data_path), reverse = False):
-        if file_name.endswith('.csv'):
+
+        file_name_extended = file_name.replace('.csv', '_Extended.csv')
+
+        if file_name.endswith('.csv') and file_name_extended not in config['exclude_file']:
             # Check if file is already present inside the 'clean_data_extended' folder
             data_path = config['data_path']
 
@@ -22,10 +25,20 @@ def build_extended_clean_data(config):
     for csv_name in csv_files_to_append:
         print(f':: -- Working on {csv_name}.csv ...')
         append_to_extended_datafile_main(csv_name, config)
-        print('test')
+        # time.sleep(60 * 2)
     
     # Create the extended datafiles
     for csv_name in csv_files_to_create:
         print(f':: -- Working on {csv_name}.csv ...')
         create_extended_datafile_main(csv_name, config)
+    
+        data_path = config['data_path']
+        clean_data_path = config['clean_data_path']
+        file = f'{data_path}/{csv_name}_Extended.csv'
+        df_extended = pd.read_csv(file)
+
+        df_extended_modified = df_extended.iloc[:-17]
+        df_extended_modified.to_csv(file, mode='w', index=False)
+
+        time.sleep(60 * 2)
     
