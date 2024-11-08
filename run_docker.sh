c21e8787a8bfc0c4e1cd307ef6cff1482986a1c4
Luke Macy <lukemacy1@gmail.com>
2024-11-07 20:42:14 -0700

    updated run docker script to follow logs

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "killing old docker processes"
-docker-compose rm -fs
+docker compose rm -fs
 
 echo "building docker containers"
-docker-compose up --build -d
+docker compose build && \
+docker compose up --detach && \
+docker compose logs --follow
